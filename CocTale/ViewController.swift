be5503a0dd24ce542a050c7f0c372c91e55ed243
zhixuan mo <zmo1995@outlook.com>
2020-09-08 18:00:59 -0400

    Initial Commit

---
+++ CocTale/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CocTale
+//
+//  Created by ZHI XUAN MO on 9/8/20.
+//  Copyright © 2020 zhixuan mo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
